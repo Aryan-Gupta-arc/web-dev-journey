887daf2c3900566a25d6b6dea7306af69f94f992
Aryan-Gupta-arc <aryangupta.arc@gmail.com>
2026-06-23 20:09:19 +0530

    Add files via upload

---
+++ Data_types.js
@@ -0,0 +1,20 @@
+"Use Strict"
+
+let arr = ["Aman","Aryan" , "Dj"]
+
+let obj = {
+    name:"0Aryan" ,
+    age: 18 ,
+    Sex: "Male"    
+}
+
+let a=9
+let b=1
+const Myfn = function(a,b){
+ console.log(a+b)
+}
+
+console.log(obj)
+console.log(typeof obj)
+Myfn
+ 
---
+++ date.js
@@ -0,0 +1,16 @@
+
+const mydate = new Date()
+//console.log(mydate.toDateString())
+//console.log(mydate.toTimeString())
+//console.log(mydate.toISOString())
+//console.log(mydate.getFullYear())
+
+let mycreateddate = new Date( 2002 , 7 , 27  , 13 ,58)
+//console.log(mycreateddate.toLocaleTimeString())
+
+let timestamp = Date.now()
+
+console.log(timestamp)
+console.log(mycreateddate.getTime())
+
+console.log('Coding is kind of booring') 
